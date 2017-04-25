f250baec920f42df7135f21370e520402357babe
pooi <ldayou@naver.com>
2017-04-25 10:27:16 +0900

    버그 수정   * 프로필보기에서 조회가 완료되지 않은 상태에서 프로필보기를 종료하면 앱이 죽던문제 수정 (fixed #1)

--- app/src/main/java/ga/findparty/findparty/profile/InfoFragment.java
+++ app/src/main/java/ga/findparty/findparty/profile/InfoFragment.java
@@ -162,82 +162,90 @@ public class InfoFragment extends Fragment {
     }
 
     private void setParentActivityUI(){
-        ((ProfileActivity)getActivity()).setProfile((String)item.get("name"), (String)item.get("img"));
+        try {
+            ((ProfileActivity) getActivity()).setProfile((String) item.get("name"), (String) item.get("img"));
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 
     private void setInterestField(){
 
-        ArrayList<String> interest = (ArrayList<String>)item.get("interest");
-        loadingInterest.hide();
+        try {
+            ArrayList<String> interest = (ArrayList<String>) item.get("interest");
+            loadingInterest.hide();
 
-        interestField.removeAllViews();
+            interestField.removeAllViews();
 
-        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
-        float dpHeight = displayMetrics.heightPixels / displayMetrics.density;
-        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
-        dpWidth -= 85;
+            DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+            float dpHeight = displayMetrics.heightPixels / displayMetrics.density;
+            float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+            dpWidth -= 85;
 
-        final float scale =getResources().getDisplayMetrics().density;
-        int width = (int) (dpWidth * scale + 0.5f);
-        int dp5 = (int) (5 * scale + 0.5f);
-        int dp3 = (int) (3 * scale * 0.5f);
+            final float scale = getResources().getDisplayMetrics().density;
+            int width = (int) (dpWidth * scale + 0.5f);
+            int dp5 = (int) (5 * scale + 0.5f);
+            int dp3 = (int) (3 * scale * 0.5f);
 
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        params.setMargins(dp3, dp3, dp3, dp3);
-        int mButtonsSize = 0;
-        Rect bounds = new Rect();
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(dp3, dp3, dp3, dp3);
+            int mButtonsSize = 0;
+            Rect bounds = new Rect();
 
-        boolean isAdd = false;
-        TextView finalText = new TextView(context);
-        finalText.setPadding(dp5*2, dp5, dp5*2, dp5);
-        finalText.setBackgroundResource(R.drawable.round_button_blue);
-        finalText.setTextColor(ContextCompat.getColor(context, R.color.white));
-        finalText.setText("+" + interest.size());
-        finalText.setLayoutParams(params);
-        finalText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //redirectSelectActivity();
+            boolean isAdd = false;
+            TextView finalText = new TextView(context);
+            finalText.setPadding(dp5 * 2, dp5, dp5 * 2, dp5);
+            finalText.setBackgroundResource(R.drawable.round_button_blue);
+            finalText.setTextColor(ContextCompat.getColor(context, R.color.white));
+            finalText.setText("+" + interest.size());
+            finalText.setLayoutParams(params);
+            finalText.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //redirectSelectActivity();
+                }
+            });
+
+            for (int i = 0; i < interest.size(); i++) {
+
+                int remainCount = interest.size() - i;
+                String remainString = "+" + remainCount;
+                finalText.setText(remainString);
+
+                String s = interest.get(i);
+                TextView mBtn = new TextView(context);
+                mBtn.setPadding(dp5 * 2, dp5, dp5 * 2, dp5);
+                mBtn.setBackgroundResource(R.drawable.round_button_blue_line);
+                mBtn.setTextColor(ContextCompat.getColor(context, R.color.pastel_blue));
+                mBtn.setText(s);
+                mBtn.setLayoutParams(params);
+
+                Paint textPaint = mBtn.getPaint();
+                textPaint.getTextBounds(s, 0, s.length(), bounds);
+                int textWidth = bounds.width() + dp5 * 4 + dp3 * 2;
+
+                Rect tempBounds = new Rect();
+                textPaint = finalText.getPaint();
+                textPaint.getTextBounds(remainString, 0, remainString.length(), tempBounds);
+                int remainWidth = tempBounds.width() + dp5 * 4 + dp3 * 2;
+
+                if (mButtonsSize + textWidth + remainWidth < width) {
+                    interestField.addView(mBtn);
+                    mButtonsSize += textWidth;
+                } else {
+                    interestField.addView(finalText);
+                    isAdd = true;
+                    break;
+                }
+
             }
-        });
 
-        for(int i=0; i<interest.size(); i++){
-
-            int remainCount = interest.size() - i;
-            String remainString = "+" + remainCount;
-            finalText.setText(remainString);
-
-            String s = interest.get(i);
-            TextView mBtn = new TextView(context);
-            mBtn.setPadding(dp5*2, dp5, dp5*2, dp5);
-            mBtn.setBackgroundResource(R.drawable.round_button_blue_line);
-            mBtn.setTextColor(ContextCompat.getColor(context, R.color.pastel_blue));
-            mBtn.setText(s);
-            mBtn.setLayoutParams(params);
-
-            Paint textPaint = mBtn.getPaint();
-            textPaint.getTextBounds(s, 0, s.length(), bounds);
-            int textWidth = bounds.width() + dp5*4 + dp3*2;
-
-            Rect tempBounds = new Rect();
-            textPaint = finalText.getPaint();
-            textPaint.getTextBounds(remainString, 0, remainString.length(), tempBounds);
-            int remainWidth = tempBounds.width() + dp5*4 + dp3*2;
-
-            if(mButtonsSize + textWidth + remainWidth < width){
-                interestField.addView(mBtn);
-                mButtonsSize += textWidth;
-            }else{
+            if (!isAdd) {
+                finalText.setText("+0");
                 interestField.addView(finalText);
-                isAdd = true;
-                break;
             }
-
-        }
-
-        if(!isAdd){
-            finalText.setText("+0");
-            interestField.addView(finalText);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
         }
 
     }
