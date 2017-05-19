01dc992f632943ee8acde80437aeda86983da1bf
pooi <ldayou@naver.com>
2017-05-19 11:31:02 +0900

    기능 추가   * 비공개 평가는 보여주지 않도록 함   * 비공개 평가가 있으면 붉은 글씨로 현재 비공개 평가의 개수를 알려줌   * 평가 항목이 여러개일때 %가 제대로 보이지 않던 문제 수정(fixed #3)

--- app/src/main/java/ga/findparty/findparty/profile/ReviewFragment.java
+++ app/src/main/java/ga/findparty/findparty/profile/ReviewFragment.java
@@ -43,9 +43,11 @@ public class ReviewFragment extends BaseFragment {
     private TextView tv_msg;
     private RelativeLayout rl_detailBtn;
     private TextView tv_countMsg;
+    private TextView tv_secretCountMsg;
     private LinearLayout li_listField;
 
     private String userId;
+    private int secretCount=0;
 
     private HashMap<String, Object> originalRatingList;
     private HashMap<String, Object> ratingList;
@@ -87,6 +89,8 @@ public class ReviewFragment extends BaseFragment {
         loading = (AVLoadingIndicatorView)view.findViewById(R.id.loading);
         tv_msg = (TextView)view.findViewById(R.id.tv_msg);
         tv_msg.setVisibility(View.GONE);
+        tv_secretCountMsg = (TextView)view.findViewById(R.id.tv_secret_count_msg);
+        tv_secretCountMsg.setVisibility(View.GONE);
         rl_detailBtn = (RelativeLayout)view.findViewById(R.id.rl_detail_btn);
         tv_countMsg = (TextView)view.findViewById(R.id.tv_count_msg);
         tv_countMsg.setVisibility(View.GONE);
@@ -110,9 +114,15 @@ public class ReviewFragment extends BaseFragment {
 
         li_listField.removeAllViews();
 
-        tv_countMsg.setText(String.format("현재 %d개의 평가가 있습니다.", reviewList.size()));
+        tv_countMsg.setText(String.format("현재 총 %d개의 평가가 있습니다.", reviewList.size()));
         tv_countMsg.setVisibility(View.VISIBLE);
         li_listField.addView(tv_countMsg);
+
+        if(secretCount > 0){
+            tv_secretCountMsg.setText(String.format("현재 %d개의 비공개 평가가 있습니다.", secretCount));
+            tv_secretCountMsg.setVisibility(View.VISIBLE);
+            li_listField.addView(tv_secretCountMsg);
+        }
 
         String[] keyList = new String[ratingList.size()];
         ratingList.keySet().toArray(keyList);
@@ -132,11 +142,12 @@ public class ReviewFragment extends BaseFragment {
                 String index = "가나다라마바사아자차카타파하";
                 for (int j = 0; j < answerList.length; j++) {
 
-                    int percentage = 0;
+                    double percentage = 0;
                     if (total > 0) {
-                        percentage = (int) ((double) answerCount[j] / total) * 100;
+                        percentage = ((double) answerCount[j] / total) * 100.0;
                     }
-                    String s = index.charAt(j) + ". " + answerList[j] + "(" + percentage + "%)";
+                    String s = String.format("%s. %s(%.1f%%)", index.charAt(j), answerList[j], percentage);
+                    //String s = index.charAt(j) + ". " + answerList[j] + "(" + percentage + "%)";
 
                     if (j < answerList.length - 1) {
                         s += "\n";
@@ -168,33 +179,43 @@ public class ReviewFragment extends BaseFragment {
 
     private void makeData(){
 
+        secretCount = 0;
+
         for(HashMap<String, Object> h : reviewList){
 
-            ArrayList<String[]> content = (ArrayList<String[]>)h.get("content");
-            for(String[] data : content){
+            boolean isSecret = (boolean)h.get("isSecret");
 
-                String id = data[0];
-                int index = Integer.parseInt(data[1]);
+            if(isSecret){
+                secretCount += 1;
+            }
+            else {
 
-                if(ratingList.containsKey(id)){
+                ArrayList<String[]> content = (ArrayList<String[]>) h.get("content");
+                for (String[] data : content) {
 
-                    HashMap<String, Object> map = (HashMap<String, Object>)ratingList.get(id);
+                    String id = data[0];
+                    int index = Integer.parseInt(data[1]);
 
-                    int total = (int)map.get("total");
-                    int[] answerCount = (int[])map.get("answerCount");
+                    if (ratingList.containsKey(id)) {
 
-                    if(index < answerCount.length) {
-                        answerCount[index] += 1;
-                        total += 1;
+                        HashMap<String, Object> map = (HashMap<String, Object>) ratingList.get(id);
 
-                        map.put("total", total);
-                        map.put("answerCount", answerCount);
+                        int total = (int) map.get("total");
+                        int[] answerCount = (int[]) map.get("answerCount");
 
-                        ratingList.put(id, map);
+                        if (index < answerCount.length) {
+                            answerCount[index] += 1;
+                            total += 1;
+
+                            map.put("total", total);
+                            map.put("answerCount", answerCount);
+
+                            ratingList.put(id, map);
+                        }
+
                     }
 
                 }
-
             }
 
         }
